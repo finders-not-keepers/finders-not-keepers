01d393da6ec8ebb490aa44f6458e892998849f5a
Katarina <klingat@me.com>
2016-08-16 20:10:59 +0000

    merge

--- src/js/components/ClaimItemForm.jsx
+++ src/js/components/ClaimItemForm.jsx
@@ -2,17 +2,21 @@ var React = require('react');
 var axios = require("axios");
 
 var ClaimItemForm = React.createClass ({
+    _handleClick: function() {
+        // send to primary account holder e-mail
+    },
     render: function() {
         return (
             <div>
-            <h2>Claim... {/*name of item*/}</h2>
+            <h2>Claim...(*name of item*)</h2>
+            <h3>include picture of item here</h3>
             <p>We highly suggest reading our guidelines, <a href="#">here</a>, for claiming an item if this is your first time.</p>
                 <form>
                     <input placeholder="First name"/>
                     <input placeholder="Last name"/>
                     <textarea ref="descriptionInput" placeholder="Description"></textarea>
                     <input placeholder="Phone number" />
-                    <button>Send!</button>
+                    <button onClick={this._handleClick}>Send!</button>
                 </form>
             </div>
             )
