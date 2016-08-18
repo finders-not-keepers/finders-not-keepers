6ea85faa7ec09ab1a989b80835e4867539d839b0
Celine Broomhead <celine.broomhead@gmail.com>
2016-08-18 23:08:20 +0000

    reverted routes

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,5 +1,3 @@
-/*global localStorage */
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
@@ -11,53 +9,34 @@ var IndexRoute = ReactRouter.IndexRoute;
 //project files
 var SearchAccount = require("./components/SearchAccount.jsx")
 var AccountSearch = require("./components/AccountSearch.jsx")
+var LogIn = require("./components/LogIn.jsx")
 var SignUp = require("./components/SignUp.jsx")
 var SearchItem = require("./components/SearchItem.jsx")
 var Items = require("./components/Items.jsx")
 var AccountPage = require("./components/AccountPage.jsx")
 var CreatePost = require("./components/CreatePost.jsx")
 var FAQ = require("./components/FAQ.jsx")
-var ClaimItemForm = require("./components/ClaimItemForm.jsx")
-var Guidelines = require("./components/Guidelines.jsx")
-import AuthService from "./utils/AuthService";
-var Logout = require('./components/Logout');
 
 var App = require("./components/App.jsx");
-var auth = new AuthService("PmdbxTpKHsOulN583eoykb8Z8lizNulQ", "findersnotkeepers.auth0.com");
-
-var requireAuth = function(nextState, replace, next) {
-    if (!auth.loggedIn()) {
-        localStorage.setItem('last_url', window.location.pathname);
-        replace({ pathname: '/' + window.location.hash})
-    }
-    
-    
-    next();  
-}
-
-
 
 var routes = (
     <Router history={ReactRouter.browserHistory}>
-        <Route path="/" component={App} auth={auth}>
-        <IndexRoute component={SearchAccount} />
+        <Route path="/" component={App}>
+        <IndexRoute component={SearchAccount}/>
             <Route path="account/:username" component={AccountSearch}>
                 <Route path="searchItem" component={SearchItem}>
-                    <Route path="items/:query" component={Items}/>
+                    <Route path="items/:query" component={Items} />
                 </Route>
             </Route>
-         
-            <Route path="accountPage" component={AccountPage} onEnter={requireAuth} />
+            
+            <Route path="login" component={LogIn}/> 
+            <Route path="accountPage" component={AccountPage} />
             <Route path="signup" component={SignUp} />
-            <Route path="createPost" component={CreatePost} onEnter={requireAuth} />
+            <Route path="createPost" component={CreatePost} />
             <Route path="FAQ" component={FAQ} />
-            <Route path="claimItem/:itemId" component={ClaimItemForm}  />
-            <Route path="guidelines" component={Guidelines} />
-            <Route path="logout" component={Logout} />
         </Route>
         
     </Router>
 );
 
 module.exports = routes;
-
